73df3e46dc426090217891a986517cf078748893
rtthoma3 <rtthoma3@ncsu.edu>
2024-10-29 16:27:44 -0400

    Cleaned up testing workflow and testing seup

--- Code/backend/test_resources/setup.js
+++ Code/backend/test_resources/setup.js
@@ -1,8 +1,6 @@
 import { TextEncoder, TextDecoder } from 'util';
 import mongodb from "mongodb"
 const MongoClient = mongodb.MongoClient;
-// import recipes from "./testRecipes.json" assert {type: 'json'}
-// import users from "./testUsers.json" assert {type: 'json'}
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -10,50 +8,50 @@ export default function (globalConfig, projectConfig) {
   
     console.log('Setting up for tests')
     const users = {"users": [
-        {
-            "userName": "Test",
-            "password": "admin"
-        }
+      {
+        "userName": "Test",
+        "password": "admin"
+      }
     ]}
     const recipes = {
       "recipes": [
-          {
-              "TranslatedRecipeName": "BLT",
-              "TotalTimeInMins": "15",
-              "Diet-type": "",
-              "Recipe-rating": 5,
-              "Times-rated": 1,
-              "Cuisine": "",
-              "image-url": "",
-              "URL": "",
-              "TranslatedInstructions": "Cook sandwich",
-              "Cleaned-Ingredients": "Bacon%Lettuce%Tomato%Bread%",
-              "Restaurant": "",
-              "Restaurant-Location": ""
-          },
-          {
-              "TranslatedRecipeName": "Andhra",
-              "TotalTimeInMins": "20",
-              "Diet-type": "Vegetarian",
-              "Recipe-rating": 5,
-              "Times-rated": 1,
-              "Cuisine": "Indian",
-              "image-url": "",
-              "URL": "",
-              "TranslatedInstructions": "Cook the food",
-              "Cleaned-Ingredients": "Mango%Rice%",
-              "Restaurant": "",
-              "Restaurant-Location": ""
-          }
+        {
+          "TranslatedRecipeName": "BLT",
+          "TotalTimeInMins": "15",
+          "Diet-type": "",
+          "Recipe-rating": 5,
+          "Times-rated": 1,
+          "Cuisine": "",
+          "image-url": "",
+          "URL": "",
+          "TranslatedInstructions": "Cook sandwich",
+          "Cleaned-Ingredients": "Bacon%Lettuce%Tomato%Bread%",
+          "Restaurant": "",
+          "Restaurant-Location": ""
+        },
+        {
+          "TranslatedRecipeName": "Andhra",
+          "TotalTimeInMins": "20",
+          "Diet-type": "Vegetarian",
+          "Recipe-rating": 5,
+          "Times-rated": 1,
+          "Cuisine": "Indian",
+          "image-url": "",
+          "URL": "",
+          "TranslatedInstructions": "Cook the food",
+          "Cleaned-Ingredients": "Mango%Rice%",
+          "Restaurant": "",
+          "Restaurant-Location": ""
+        }
       ]
-  }
+    }
     Object.assign(global, { TextDecoder, TextEncoder });
     const uri = process.env.RECIPES_DB_URI;
     var mongoClient = MongoClient.connect(uri, {
         useNewUrlParser: true,
         maxPoolSize: 50,
         wtimeoutMS: 2500,
-      }).then(async (client) => {
+    }).then(async (client) => {
         const recipeCollection = client.db(process.env.RECIPES_NS).collection("recipe")//.then(async (recipeCollection) => {
         await recipeCollection.deleteMany({})
         await recipeCollection.insertMany(recipes.recipes)
@@ -61,5 +59,5 @@ export default function (globalConfig, projectConfig) {
         await userCollection.deleteMany({})
         await userCollection.insertMany(users.users)
         client.close()
-        });
+    });
 };
