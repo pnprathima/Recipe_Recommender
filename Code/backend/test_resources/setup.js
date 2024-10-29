37b2bad8298a71cbae092ae84f424d31a1f994d5
rtthoma3 <rtthoma3@ncsu.edu>
2024-10-29 16:23:04 -0400

    Adjustments to input data in test setup

--- Code/backend/test_resources/setup.js
+++ Code/backend/test_resources/setup.js
@@ -1,14 +1,52 @@
 import { TextEncoder, TextDecoder } from 'util';
 import mongodb from "mongodb"
 const MongoClient = mongodb.MongoClient;
-import recipes from "./testRecipes.json" assert {type: 'json'}
-import users from "./testUsers.json" assert {type: 'json'}
+// import recipes from "./testRecipes.json" assert {type: 'json'}
+// import users from "./testUsers.json" assert {type: 'json'}
 import dotenv from 'dotenv'
 dotenv.config()
 
 export default function (globalConfig, projectConfig) {
   
     console.log('Setting up for tests')
+    const users = {"users": [
+        {
+            "userName": "Test",
+            "password": "admin"
+        }
+    ]}
+    const recipes = {
+      "recipes": [
+          {
+              "TranslatedRecipeName": "BLT",
+              "TotalTimeInMins": "15",
+              "Diet-type": "",
+              "Recipe-rating": 5,
+              "Times-rated": 1,
+              "Cuisine": "",
+              "image-url": "",
+              "URL": "",
+              "TranslatedInstructions": "Cook sandwich",
+              "Cleaned-Ingredients": "Bacon%Lettuce%Tomato%Bread%",
+              "Restaurant": "",
+              "Restaurant-Location": ""
+          },
+          {
+              "TranslatedRecipeName": "Andhra",
+              "TotalTimeInMins": "20",
+              "Diet-type": "Vegetarian",
+              "Recipe-rating": 5,
+              "Times-rated": 1,
+              "Cuisine": "Indian",
+              "image-url": "",
+              "URL": "",
+              "TranslatedInstructions": "Cook the food",
+              "Cleaned-Ingredients": "Mango%Rice%",
+              "Restaurant": "",
+              "Restaurant-Location": ""
+          }
+      ]
+  }
     Object.assign(global, { TextDecoder, TextEncoder });
     const uri = process.env.RECIPES_DB_URI;
     var mongoClient = MongoClient.connect(uri, {
