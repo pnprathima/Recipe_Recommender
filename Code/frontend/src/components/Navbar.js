f6722c3a7af21779e250795a0b484b6516e79c34
NIKITA CHAUDHARI <iamnikitachaudhari@gmail.com>
2024-11-02 02:39:07 -0400

    Added logo and website name.

--- Code/frontend/src/components/Navbar.js
+++ Code/frontend/src/components/Navbar.js
@@ -61,7 +61,9 @@ export default function Nav(props) {
     <>
       <Box color={"black"} mb={5}  bg={"purple"} px={5}>
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          
+        <Link href="/" pl={10}> {/* Link to your homepage */}
+            <Image src="/newlogo.png" alt="Logo" boxSize="50px" />
+          </Link>
           <Box pl={10}><Heading size={"md"} color={"white"}>Saveurs Sélection</Heading></Box>
 
           <Flex alignItems={'center'}>
@@ -79,7 +81,7 @@ export default function Nav(props) {
                   minW={0}>
                   <Avatar
                     size={'sm'}
-                    src={'https://avatars.dicebear.com/api/male/username.svg'}
+                    src={'https://i.redd.it/fcy75bgri5u41.jpg'}
                   />
                 </MenuButton>
                 <MenuList alignItems={'center'}>
@@ -87,7 +89,7 @@ export default function Nav(props) {
                   <Center>
                     <Avatar
                       size={'xl'}
-                      src={'https://avatars.dicebear.com/api/male/username.svg'}
+                      src={'https://i.redd.it/fcy75bgri5u41.jpg'}
                     />
                   </Center>
                   <br />
