9b2a6116ae78467c22d38696dbfa0aa90c178abd
tejasdesai27 <me.tejasdesai@gmail.com>
2024-11-02 16:44:57 -0400

    Integrate diet_type filtering feature in main application flow

--- Code/frontend/src/App.js
+++ Code/frontend/src/App.js
@@ -1,4 +1,3 @@
-//  
 import Form from "./components/Form.js";
 import Header from "./components/Header";
 import recipeDB from "./apis/recipeDB";
@@ -20,7 +19,6 @@ class App extends Component {
 
     this.state = {
       cuisine: "",
-      //NoIngredients : 0,
       ingredients: new Set(),
       recipeList: [],
       recipeByNameList: [],
@@ -30,84 +28,84 @@ class App extends Component {
       isLoading: false,
       isLoggedIn: false,
       isProfileView: false,
-      userData: {}
+      userData: {},
+      dietType: "", // Added state for diet type
     };
   }
 
-  handleBookMarks = ()=> {
+  handleBookMarks = () => {
     this.setState({
-      isProfileView: true
-    })
-  }
+      isProfileView: true,
+    });
+  };
 
-  handleProfileView = ()=> {
+  handleProfileView = () => {
     this.setState({
-      isProfileView: false
-    })
-  }
+      isProfileView: false,
+    });
+  };
 
-  handleSignup = async (userName, password)=> {
+  handleSignup = async (userName, password) => {
     try {
       const response = await recipeDB.post("/recipes/signup", {
-          userName,
-          password
+        userName,
+        password,
       });
-      console.log(response.data)
+      console.log(response.data);
       if (response.data.success) {
-        alert("Successfully Signed up!")
+        alert("Successfully Signed up!");
         this.setState({
           isLoggedIn: true,
-          userData: response.data.user
-        })
-        localStorage.setItem("userName", response.data.user.userName)
-        console.log(response.data.user)
+          userData: response.data.user,
+        });
+        localStorage.setItem("userName", response.data.user.userName);
+        console.log(response.data.user);
       } else {
-        alert("User already exists")
+        alert("User already exists");
       }
     } catch (err) {
       console.log(err);
     }
-  }
+  };
 
   handleLogin = async (userName, password) => {
     try {
       const response = await recipeDB.get("/recipes/login", {
         params: {
           userName,
-          password
+          password,
         },
       });
-      console.log(response.data)
+      console.log(response.data);
       if (response.data.success) {
         this.setState({
           isLoggedIn: true,
-          userData: response.data.user
-        })
-        localStorage.setItem("userName", response.data.user.userName)
-        console.log(response.data.user)
-        alert("Successfully logged in!")
+          userData: response.data.user,
+        });
+        localStorage.setItem("userName", response.data.user.userName);
+        console.log(response.data.user);
+        alert("Successfully logged in!");
       } else {
-        console.log("Credentials are incorrect")
+        console.log("Credentials are incorrect");
       }
     } catch (err) {
       console.log(err);
     }
-  }
+  };
 
   // Function to get the user input from the Form component on Submit action
   handleSubmit = async (formDict) => {
     this.setState({
-      isLoading: true
-    })
-    console.log(formDict)
+      isLoading: true,
+    });
+    console.log(formDict);
     this.setState({
-      // cuisine: cuisineInput,
-      //NoIngredients: noIngredientsInput,
       ingredients: formDict["ingredient"],
       cuisine: formDict["cuisine"],
       email: formDict["email_id"],
       cookingTime: formDict["cooking_time"],
       flag: formDict["flag"],
+      dietType: formDict["diet_type"], // Update for diet type
     });
 
     const mail = formDict["email_id"];
@@ -115,29 +113,31 @@ class App extends Component {
     const items = Array.from(formDict["ingredient"]);
     const cuis = formDict["cuisine"];
     const cookingTime = formDict["cooking_time"];
-    this.getRecipeDetails(items, cuis, mail, flag, cookingTime);
-    //  alert(typeof(ingredientsInput["cuisine"]));
+    const dietType = formDict["diet_type"]; // Update for diet type
+    this.getRecipeDetails(items, cuis, mail, flag, cookingTime, dietType); // Update to include diet type
   };
 
   handleRecipesByName = (recipeName, cookingTime) => {
     this.setState({
-      isLoading: true
-    })
-    recipeDB.get("/recipes/getRecipeByName", {
-      params: {
-        recipeName: recipeName,
-        TotalTimeInMins: cookingTime
-      }
-    }).then(res => {
-      console.log(res.data);
-      this.setState({
-        recipeByNameList: res.data.recipes,
-        isLoading: false
+      isLoading: true,
+    });
+    recipeDB
+      .get("/recipes/getRecipeByName", {
+        params: {
+          recipeName: recipeName,
+          TotalTimeInMins: cookingTime,
+        },
       })
-    })
-  }
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          recipeByNameList: res.data.recipes,
+          isLoading: false,
+        });
+      });
+  };
 
-  getRecipeDetails = async (ingredient, cuis, mail, flag, cookingTime) => {
+  getRecipeDetails = async (ingredient, cuis, mail, flag, cookingTime, dietType) => { // Update to include diet type
     try {
       const response = await recipeDB.get("/recipes", {
         params: {
@@ -145,35 +145,43 @@ class App extends Component {
           Cuisine: cuis,
           Email: mail,
           Flag: flag,
-          TotalTimeInMins: cookingTime
+          TotalTimeInMins: cookingTime,
+          DietType: dietType, // Pass the diet type to the request
         },
       });
       this.setState({
         recipeList: response.data.recipes,
-        isLoading: false
+        isLoading: false,
       });
     } catch (err) {
       console.log(err);
     }
   };
 
-  handleLogout = ()=> {
-    console.log("logged out")
+  handleLogout = () => {
+    console.log("logged out");
     this.setState({
-      isLoggedIn: false
-    })
-  }
+      isLoggedIn: false,
+    });
+  };
 
   render() {
     return (
       <div>
-        <Nav handleLogout={this.handleLogout} handleBookMarks={this.handleBookMarks} user={this.state.userData}/>
-        {this.state.isLoggedIn ?
+        <Nav
+          handleLogout={this.handleLogout}
+          handleBookMarks={this.handleBookMarks}
+          user={this.state.userData}
+        />
+        {this.state.isLoggedIn ? (
           <>
-            {this.state.isProfileView ?
-            <UserProfile handleProfileView={this.handleProfileView} user={this.state.userData} />
-            :
-              <Tabs variant='soft-rounded' colorScheme='purple'>
+            {this.state.isProfileView ? (
+              <UserProfile
+                handleProfileView={this.handleProfileView}
+                user={this.state.userData}
+              />
+            ) : (
+              <Tabs variant="soft-rounded" colorScheme="purple">
                 <TabList ml={10}>
                   <Tab>Search Recipe</Tab>
                   <Tab>Add Recipe</Tab>
@@ -183,7 +191,11 @@ class App extends Component {
                   <TabPanel>
                     <Box display="flex">
                       <Form sendFormData={this.handleSubmit} />
-                      {this.state.isLoading ? <RecipeLoading /> : <RecipeList recipes={this.state.recipeList} />}
+                      {this.state.isLoading ? (
+                        <RecipeLoading />
+                      ) : (
+                        <RecipeList recipes={this.state.recipeList} />
+                      )}
                     </Box>
                   </TabPanel>
                   <TabPanel>
@@ -191,21 +203,22 @@ class App extends Component {
                   </TabPanel>
                   <TabPanel>
                     <SearchByRecipe sendRecipeData={this.handleRecipesByName} />
-                    {this.state.isLoading ? <RecipeLoading /> : <RecipeList recipes={this.state.recipeByNameList} />}
+                    {this.state.isLoading ? (
+                      <RecipeLoading />
+                    ) : (
+                      <RecipeList recipes={this.state.recipeByNameList} />
+                    )}
                   </TabPanel>
                 </TabPanels>
               </Tabs>
-            }
+            )}
           </>
-          : <Login handleSignup={this.handleSignup} handleLogin={this.handleLogin} />
-        }
-        {/* handleSubmit function is being sent as a prop to the form component*/}
-
-
-        {/* RecipeList is the component where results are displayed.
-        App's recipeList state item is being sent as a prop
-        */}
-
+        ) : (
+          <Login
+            handleSignup={this.handleSignup}
+            handleLogin={this.handleLogin}
+          />
+        )}
       </div>
     );
   }
--- Code/frontend/src/components/RecipeList.js
+++ Code/frontend/src/components/RecipeList.js
@@ -1,43 +1,84 @@
 import React, { useState } from "react";
-import { Avatar, Flex,Input, Modal, ModalBody, ModalCloseButton, ModalOverlay, ModalHeader, ModalFooter, ModalContent, Box, SimpleGrid, Text, Button } from "@chakra-ui/react"
+import {
+  Avatar,
+  Flex,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalOverlay,
+  ModalHeader,
+  ModalFooter,
+  ModalContent,
+  Box,
+  SimpleGrid,
+  Text,
+  Button,
+  Select,
+} from "@chakra-ui/react";
 import RecipeCard from "./RecipeCard";
 
 // component to handle all the recipes
 const RecipeList = ({ recipes }) => {
-  // mapping each recipe item to the Recipe container
-  // const renderedRecipes = recipes.map((recipe) => {
-  //   // return <Recipe key={recipe._id} recipe={recipe} />;
-  //   return(
-
-  //   )
-  // });
-  console.log(recipes)
+  console.log(recipes);
   const [isOpen, setIsOpen] = useState(false);
   const [currentRecipe, setCurrentRecipe] = useState({});
   const [cookingTimeFilter, setCookingTimeFilter] = useState("");
+  const [dietTypeFilter, setDietTypeFilter] = useState("");
+
   var youtube_videos =
     "https://www.youtube.com/results?search_query=" +
     currentRecipe["TranslatedRecipeName"];
+
   const handleViewRecipe = (data) => {
-    setIsOpen(true)
-    console.log(data)
+    setIsOpen(true);
+    console.log(data);
     setCurrentRecipe(data);
-  }
+  };
+
   const onClose = () => {
-    setIsOpen(false)
-  }
+    setIsOpen(false);
+  };
+
   const handleCookingTimeFilterChange = (event) => {
     setCookingTimeFilter(event.target.value);
   };
 
+  const handleDietTypeFilterChange = (event) => {
+    setDietTypeFilter(event.target.value);
+  };
+
   const filterRecipesByCookingTime = (recipes) => {
     if (!cookingTimeFilter) return recipes;
-    return recipes.filter(recipe => parseInt(recipe.TotalTimeInMins) <= parseInt(cookingTimeFilter));
+    return recipes.filter(
+      (recipe) =>
+        parseInt(recipe.TotalTimeInMins) <= parseInt(cookingTimeFilter)
+    );
   };
+
+  const filterRecipesByDietType = (recipes) => {
+    if (!dietTypeFilter) return recipes;
+    return recipes.filter(
+      (recipe) => recipe["Diet-type"] === dietTypeFilter
+    );
+  };
+
   // all the recipes are being returned in the form of a table
+  const filteredRecipes = filterRecipesByCookingTime(recipes);
+  const finalFilteredRecipes = filterRecipesByDietType(filteredRecipes);
+
   return (
     <>
-      <Box borderRadius={"lg"} border="1px" boxShadow={"10px"} borderColor={"gray.100"} fontFamily="regular" m={10} width={"70%"} p={5}>
+      <Box
+        borderRadius={"lg"}
+        border="1px"
+        boxShadow={"10px"}
+        borderColor={"gray.100"}
+        fontFamily="regular"
+        m={10}
+        width={"70%"}
+        p={5}
+      >
         <Box mb={4}>
           <Text mb={2}>Filter by Cooking Time (minutes):</Text>
           <Input
@@ -47,38 +88,85 @@ const RecipeList = ({ recipes }) => {
             placeholder="Max cooking time"
           />
         </Box>
-        <SimpleGrid spacing={5} templateColumns='repeat(auto-fill, minmax(250px, 1fr))'>
-          {filterRecipesByCookingTime(recipes).length !== 0 ? filterRecipesByCookingTime(recipes).map((recipe) => (
-            <RecipeCard key={recipe._id} handler={handleViewRecipe} recipe={recipe} />
-          )) : <Text data-testid="noResponseText" fontSize={"lg"} color={"gray"}>No recipes found matching the criteria.</Text>}
+        <Box mb={4}>
+          <Text mb={2}>Filter by Diet Type:</Text>
+          <Select
+            placeholder="Select diet type"
+            value={dietTypeFilter}
+            onChange={handleDietTypeFilterChange}
+          >
+            <option value="Vegan">Vegan</option>
+            <option value="Vegetarian">Vegetarian</option>
+            <option value="Non-Vegetarian">Non-Vegetarian</option>
+          </Select>
+        </Box>
+        <SimpleGrid
+          spacing={5}
+          templateColumns="repeat(auto-fill, minmax(250px, 1fr))"
+        >
+          {finalFilteredRecipes.length !== 0 ? (
+            finalFilteredRecipes.map((recipe) => (
+              <RecipeCard
+                key={recipe._id}
+                handler={handleViewRecipe}
+                recipe={recipe}
+              />
+            ))
+          ) : (
+            <Text data-testid="noResponseText" fontSize={"lg"} color={"gray"}>
+              No recipes found matching the criteria.
+            </Text>
+          )}
         </SimpleGrid>
       </Box>
       <Modal size={"6xl"} isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
-        <ModalContent data-testid="recipeModal" >
+        <ModalContent data-testid="recipeModal">
           <ModalHeader>{currentRecipe.TranslatedRecipeName}</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Flex >
-            <Avatar size="2xl" mr={2} mb={2} src={currentRecipe["image-url"]} />
+            <Flex>
+              <Avatar
+                size="2xl"
+                mr={2}
+                mb={2}
+                src={currentRecipe["image-url"]}
+              />
               <Box mt={4}>
-                <Text><Text as={"b"}>Cooking Time: </Text>{currentRecipe.TotalTimeInMins} mins</Text>
-                <Text><Text as={"b"}>Rating: </Text> {currentRecipe['Recipe-rating']}</Text>
-                <Text mb={2}><Text as={"b"}>Diet Type: </Text> {currentRecipe['Diet-type']}</Text>
+                <Text>
+                  <Text as={"b"}>Cooking Time: </Text>
+                  {currentRecipe.TotalTimeInMins} mins
+                </Text>
+                <Text>
+                  <Text as={"b"}>Rating: </Text>{" "}
+                  {currentRecipe["Recipe-rating"]}
+                </Text>
+                <Text mb={2}>
+                  <Text as={"b"}>Diet Type: </Text> {currentRecipe["Diet-type"]}
+                </Text>
               </Box>
             </Flex>
-            <Text><Text as={"b"}>Instructions: </Text> {currentRecipe["TranslatedInstructions"]}</Text>
-            <Text color={"blue"}><Text color={"black"}  as={"b"}>Video Url: </Text><a href={youtube_videos}>Youtube</a></Text>
+            <Text>
+              <Text as={"b"}>Instructions: </Text>{" "}
+              {currentRecipe["TranslatedInstructions"]}
+            </Text>
+            <Text color={"blue"}>
+              <Text color={"black"} as={"b"}>
+                Video Url:{" "}
+              </Text>
+              <a href={youtube_videos}>Youtube</a>
+            </Text>
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme='teal' mr={3} onClick={onClose}>
+            <Button colorScheme="teal" mr={3} onClick={onClose}>
               Close
             </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
     </>
-  )
+  );
 };
+
 export default RecipeList;
