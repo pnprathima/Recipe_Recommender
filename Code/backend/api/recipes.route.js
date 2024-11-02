30d2e892c2f9c4bac9f055a6a9377925fa31716d
tejasdesai27 <me.tejasdesai@gmail.com>
2024-11-02 16:50:11 -0400

    Enhance controller to handle diet_type filtering in recipes

--- Code/backend/api/recipes.route.js
+++ Code/backend/api/recipes.route.js
@@ -3,14 +3,14 @@ import RecipesCtrl from "./recipes.controller.js";
 import RecipesDAO from "../dao/recipesDAO.js";
 
 const router = express.Router();
-//URl to get the recipes
+//URL to get the recipes
 router.route("/").get(RecipesCtrl.apiGetRecipes);
 
 router.route("/cuisines").get(RecipesCtrl.apiGetRecipeCuisines);
 
 router.route("/addRecipe").post(RecipesCtrl.apiPostRecipe);
 
-router.route('/callIngredients').get(RecipesCtrl.apiGetIngredients);
+router.route("/callIngredients").get(RecipesCtrl.apiGetIngredients);
 
 router.route("/signup").post(RecipesCtrl.apiAuthSignup);
 
